1a774064cd662a9457b512721b57b438f5c63084
RodrigoFMC <102884339+RodrigoFMC@users.noreply.github.com>
2024-07-04 13:16:55 +0100

    Atualiza estrutura do projeto

---
+++ src/PRJ_LEIM_SINCRO/Plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+ 'id' => 'rodrigos:RadarDetailsPlugin',
+ 'version' => '1.0',
+ 'name' => 'Radar Details',
+ 'author' => 'Rodrigo Costa and Rodrigo Bugalhão',
+ 'description' => 'TODO Description',
+ 'url' => 'http://localhost/osTicket/upload/scp/plugins.php',
+ 'plugin' => 'RadarDetailsPlugin.php:RadarDetailsPlugin'
+);
